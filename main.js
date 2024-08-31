db1ee2436538b9c170114556ae01f90835db45f0
gigg88 <popp.christian88@gmail.com>
2024-08-31 23:19:05 +0300

    showing date till visit

---
+++ main.js
@@ -0,0 +1,26 @@
+// get the current date
+// Subtract the 28.09.2024 from the current date
+
+function currentDate() {
+    const today = new Date();
+
+    const dayOfMonth = today.getDate();
+    const monthOfYear = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    console.log(`${dayOfMonth} ${monthOfYear} ${year}`);
+
+}
+
+currentDate();
+
+function daysUntil(futureDate) {
+    const now = new Date();
+    const future = new Date(futureDate);
+    const differenceInMilliseconds = future - now;
+    const differenceInDays = Math.ceil(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+    return differenceInDays;
+  }
+  
+  const daysUntilSeptember28 = daysUntil("2024-09-28");
+  console.log("Days until September 28, 2024:", daysUntilSeptember28);
